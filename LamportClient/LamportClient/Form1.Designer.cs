68ee602bb07297de44a67cb93f579e62369ba986
divya141997 <40003077+divya141997@users.noreply.github.com>
2018-07-05 12:50:51 +0530

    Add files via upload

---
+++ LamportClient/LamportClient/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LamportClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.destination = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clock = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.TextBox();
+            this.serverAddress = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Message To";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // destination
+            // 
+            this.destination.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.destination.Location = new System.Drawing.Point(219, 120);
+            this.destination.Multiline = true;
+            this.destination.Name = "destination";
+            this.destination.Size = new System.Drawing.Size(166, 49);
+            this.destination.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clock :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.Location = new System.Drawing.Point(211, 36);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(39, 43);
+            this.clock.TabIndex = 3;
+            this.clock.Text = "0";
+            this.clock.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // log
+            // 
+            this.log.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log.Location = new System.Drawing.Point(13, 186);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.Size = new System.Drawing.Size(372, 107);
+            this.log.TabIndex = 4;
+            this.log.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // serverAddress
+            // 
+            this.serverAddress.Location = new System.Drawing.Point(13, 300);
+            this.serverAddress.Name = "serverAddress";
+            this.serverAddress.Size = new System.Drawing.Size(258, 20);
+            this.serverAddress.TabIndex = 5;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(277, 296);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(107, 23);
+            this.connect.TabIndex = 6;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 356);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.serverAddress);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.clock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.destination);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox destination;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.TextBox serverAddress;
+        private System.Windows.Forms.Button connect;
+    }
+}
+
---
+++ LamportClient/LamportClient/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LamportClient
+{
+    public partial class Form1 : Form
+    {
+        private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private byte[] buffer = new byte[1024];
+        private bool firstMsg = true;
+        private Form form;
+        private System.Windows.Forms.Timer timer;
+        private int time = 0;
+        private Random random = new Random();
+
+        private int id;
+
+        public Form1()
+        {
+            InitializeComponent();
+            form = this;
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = random.Next(100, 2000);
+            timer.Start();
+            timer.Tick += new EventHandler(TimerTick);
+
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            clock.Text = "" + (++time);
+        }
+                
+        private void LoopConnect(string serverAddr)
+        {
+            int attempts = 0;
+            while (!clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    clientSocket.Connect(serverAddr, 100);
+                }
+                catch (SocketException)
+                {
+                    AddStat("Connection attempts: " + attempts);
+                }
+            }
+            AddStat("Connected");
+
+            clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), clientSocket);
+        }
+
+        private void SendText(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            clientSocket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), clientSocket);
+            clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), clientSocket);
+        }
+
+        private void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuff = new byte[received];
+            Array.Copy(buffer, dataBuff, received);
+
+            string text = Encoding.ASCII.GetString(dataBuff);
+            if (firstMsg)
+            {
+                log.BeginInvoke(new MethodInvoker(() => form.Text = "Client [" + text + "]"));
+                firstMsg = false;
+                id = int.Parse(text);
+            }
+            else
+            {
+                if (text.Equals("No such client"))
+                {
+                    AddStat(text);
+                }
+                else
+                {
+                    string[] data = text.Split(' ');
+                    AddStat(data[0] + " -> " + data[1] + " : " + data[2]);
+
+                    int timestamp = int.Parse(data[2]);
+                    if (time < timestamp)
+                    {
+                        AddStat("Sending time : " + timestamp + "\tReceiving time : " + time);
+                        AddStat("Changing time to " + (timestamp + 1));
+                        time = timestamp + 1;
+                        clock.BeginInvoke(new MethodInvoker(() => clock.Text = time.ToString()));
+                        //AddStat("-----------------------------------------------");
+                    }
+                    else
+                    {
+                        AddStat("Sending time : " + timestamp + "\tReceiving time : " + time);
+                    }
+                }
+            }
+            clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), clientSocket);
+        }
+
+        public void AddStat(String s)
+        {
+            try
+            {
+                log.BeginInvoke(new MethodInvoker(() => log.AppendText(s + System.Environment.NewLine)));
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dest = destination.Text.ToString();
+            SendText(id + " " + dest + " " + time);
+            AddStat(id + " -> " + dest + " : " + time);
+            AddStat("-----------------------------------------------");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (serverAddress.Text != null || !serverAddress.Text.ToString().Equals(""))
+            {
+                LoopConnect(serverAddress.Text.ToString());
+            }
+        }
+    }
+}
